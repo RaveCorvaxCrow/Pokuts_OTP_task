7cfe8daa1b30733125663417bd7435e585e19bce
Dmitiry Shulakov <dmitriy.shulakov@all.me>
2022-08-22 16:06:57 +0300

    add comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ browser.config.browser_name = 'chrome'
 
 @pytest.fixture(scope='session', autouse=True)
 def browser_manger():
+    # set window size of browser
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.open(base_url)
